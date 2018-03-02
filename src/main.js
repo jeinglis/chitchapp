279cd5e722b4a0041391b64682b60dc5c78f5ca8
jeinglis <jamesekinglis@gmail.com>
2018-03-01 23:37:54 -0700

    inited the project

--- src/main.js
+++ src/main.js
@@ -1,35 +1,13 @@
-// import Vue from 'vue';
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
 
-// var app = new Vue({
-//     el: '#app',
-//     data: {
-//         message: 'Hello Vue!'
-//     }
-// })
-// var app2 = new Vue({
-//     el: '#app-2',
-//     data: {
-//         message: 'You loaded this page on ' + new Date().toLocaleString()
-//     }
-// })
-// var app3 = new Vue({
-//     el: '#app-3',
-//     data: {
-//         seen: true
-//     }
-// })
-// var app4 = new Vue({
-//     el: '#app-4',
-//     data: {
-//         todos: [{
-//                 text: 'Learn JavaScript'
-//             },
-//             {
-//                 text: 'Learn Vue'
-//             },
-//             {
-//                 text: 'Build something awesome'
-//             }
-//         ]
-//     }
-// })
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
