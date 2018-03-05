962c862261b88d9f78a25b3ec67b68f5aeae2a53
jeinglis <jamesekinglis@gmail.com>
2018-03-04 21:21:30 -0700

    modified components

---
+++ src/components/Input.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="Input">
+    <md-field>
+      <md-textarea v-model="textarea" placeholder="Type a Message  ***Other Commands: /nick <new nickname>,  /nickcolor <RRGGBB>***"></md-textarea>
+    </md-field>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Input',
+  data () {
+    return {
+    }
+  }
+}
+</script>
