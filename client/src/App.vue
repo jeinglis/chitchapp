b3810c1062f64192902dbbc7c66ca356d91a269f
James Inglis <jamesekinglis@gmail.com>
2018-03-05 00:43:45 -0700

    cleaned up dependencies

--- client/src/App.vue
+++ client/src/App.vue
@@ -44,7 +44,7 @@ import Vue from "vue";
 import VueMaterial from "vue-material";
 import "vue-material/dist/vue-material.min.css";
 import "vue-material/dist/theme/default-dark.css"; // This line here
-import socketio from 'socket.io';
+//import socketio from 'socket.io';
 // import VueSocketIO from 'vue-socket.io';
 
 // export const SocketInstance = socketio('http://localhost:3000');
